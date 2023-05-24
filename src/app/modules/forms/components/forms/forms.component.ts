f6975f19f42230e496a2dca8150d21e281691fff
Arnold <antaloczi.arnold@gmail.com>
2023-05-24 14:05:32 +0200

    Add Reactive forms to forms module

--- src/app/modules/forms/components/forms/forms.component.ts
+++ src/app/modules/forms/components/forms/forms.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss']
+  styleUrls: ['./forms.component.scss'],
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
 
+  typedForm = new FormGroup({
+    name: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+    email: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+    age: new FormControl<number | null>(null, Validators.required),
+  });
+
+  typedFormV2 = this.fb.group({
+    name: this.fb.nonNullable.control('', Validators.required),
+    email: this.fb.nonNullable.control('', Validators.required),
+    age: this.fb.control<number | null>(null, Validators.required),
+  });
+
+  ngOnInit(): void {
+    console.log('formok komponens betöltve');
+    console.log('form értékei alapból:', this.typedForm.value);
+    this.typedForm.controls.age.setValue(40);
+    console.log('érték beállítás után:', this.typedForm.value);
+    setTimeout(() => {
+      this.typedForm.reset();
+      console.log('form értékei reset után:', this.typedForm.value);
+    }, 5000);
+  }
 }
--- src/app/modules/forms/forms.module.ts
+++ src/app/modules/forms/forms.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { FormsRoutingModule } from './forms-routing.module';
 import { FormsComponent } from './components/forms/forms.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FormsComponent],
-  imports: [CommonModule, FormsRoutingModule],
+  imports: [CommonModule, FormsRoutingModule, ReactiveFormsModule],
 })
 export class FormsModule {}
